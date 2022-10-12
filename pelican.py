4a9b35584433338196319e1f66ae84ce7593b382
Lazlo <lazlopaul17@gmail.com>
2022-10-12 13:45:47 -0700

    Doing some documentation

--- pelican.py
+++ pelican.py
@@ -6,9 +6,14 @@ the python class for everything is set to int. Reg_types are bit or byte
 import requests
 import xmltodict
 import json
+from typing import Union
 
 class Pelican_Client():
     def __init__(self, config_file):
+        '''
+        Given a config file in json format, configure client
+        :param config_file: .json file like config_template.json
+        '''
         with open(config_file) as json_config:
             self.config_dict = json.load(json_config)
 
@@ -25,7 +30,15 @@ class Pelican_Client():
                              'object': self.object,
                              'value': self.point_list}
     
-    def get_points(self, points, thermostat_names):
+    def get_points(self, points: Union[list,str], thermostat_names: Union[list,str]) -> dict:
+        '''
+        Takes a list of points or a single point for a list of thermostats or single thermostat
+        and returns their values
+        :param points: List of point names or string point name
+        :param thermostat_names: List of thermostat names or single thermostat name:
+        :returns: ordered dictionary with values for each point
+        could optionally add thermostat_serialNo or other identifying information for the selection
+        '''
         payload = self.payload_dict.copy()
         payload['value'] = ';'.join(points)
         payload['request'] = 'get'
@@ -34,7 +47,13 @@ class Pelican_Client():
         r = requests.get(self.url,payload)
         return xmltodict.parse(r.content).get('result')
 
-    def set_points(self, point_val_dict, thermostat_names):
+    def set_points(self, point_val_dict: dict, thermostat_names: Union[list,str]) -> dict:
+        '''
+        Takes a dictionary of points and the value they will be set to for one or multiple Thermostats
+        :param point_val_dict: Dictionary of {point_name:new_value} that will be set for thermostats
+        :param thermostat_names: List of point names or single thermostat name string
+        :returns: dictionary saying whether set was successful
+        '''
         payload = self.payload_dict.copy()
         payload['value'] = ''
         for point_name, value in point_val_dict.items():
@@ -47,11 +66,19 @@ class Pelican_Client():
         return dict(xmltodict.parse(r.content))
         
     def scrape_all(self):
+        """
+        scrapes all points for all thermostats in config file
+        """
         # 'all' would also work for the selections, rather than all the names
         point_data = self.get_points(self.point_list, self.thermostat_names)
         return point_data# .get('Thermostat')
 
-    def get_selection(self,payload, thermostat_names):
+    def get_selection(self,payload: dict, thermostat_names: Union[list,str]):
+        """
+        adds a selection or selections key and value to the payload dictionary for get and set methods
+        :param payload: the payload dictionary used by get or set methods
+        :param thermostat_names: the names that will be added to the selection key
+        """
         if isinstance(thermostat_names, list):
             payload['selections'] = ';'.join([f'name:{x}' for x in thermostat_names])
         else:
